cb99b47a78c2784a86993adaee37965afcfa2594
danielgonzagat <122159390+danielgonzagat@users.noreply.github.com>
2025-08-06 15:52:55 -0300

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -1,31 +1,42 @@
-import { component$, isDev } from "@builder.io/qwik";
-import { QwikCityProvider, RouterOutlet } from "@builder.io/qwik-city";
-import { RouterHead } from "./components/router-head/router-head";
+import { component$, isDev } from '@builder.io/qwik';
+import { QwikCityProvider, RouterOutlet } from '@builder.io/qwik-city';
+import { RouterHead } from './components/router-head/router-head';
 
-import "./global.css";
+import './global.css';
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
-
   return (
     <QwikCityProvider>
       <head>
-        <meta charset="utf-8" />
+        {/* Meta padrão */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Descubra a cápsula sublingual PURAH que rejuvenesce a pele em até 10 anos, com garantia de 90 dias e estudos clínicos comprovados."
+        />
+        {/* Pré‑conexões para melhorar performance */}
+        <link rel="preconnect" href="https://purah.com.br/advpura" />
+        <link rel="preconnect" href="https://cdn-ildpfhj.nitrocdn.com" />
+        <link rel="preconnect" href="https://cdn.jsdelivr.net/" />
+        <link rel="prefetch" href="https://purah.com.br" />
+        {/* DNS prefetch para origens terceiras */}
+        <link rel="dns-prefetch" href="//purah.com.br/advpura" />
+        <link rel="dns-prefetch" href="//cdn-ildpfhj.nitrocdn.com" />
+        <link rel="dns-prefetch" href="//cdn.jsdelivr.net" />
+        <link rel="dns-prefetch" href="//connect.facebook.net" />
+        <link rel="dns-prefetch" href="//nitroscripts.com" />
+        <link rel="dns-prefetch" href="//unpkg.com" />
         {!isDev && (
-          <link
-            rel="manifest"
-            href={`${import.meta.env.BASE_URL}manifest.json`}
-          />
+          <link rel="manifest" href={`${import.meta.env.BASE_URL}manifest.json`} />
         )}
+        {/* Cabeçalho controlado pelo roteador */}
         <RouterHead />
       </head>
-      <body lang="pt_BR">
+      <body lang="pt-BR">
         <RouterOutlet />
+        {/* Script que carregará o HTML original e popula window.purahHtml */}
+        <script src="/purah.js" defer></script>
       </body>
     </QwikCityProvider>
   );
