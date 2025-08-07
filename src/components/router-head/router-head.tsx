f40d12a24177d0c39e8cc7161dcf322e1ac0ec26
danielgonzagat <122159390+danielgonzagat@users.noreply.github.com>
2025-08-07 03:28:28 -0300

    Update router-head.tsx

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,8 +1,12 @@
-import { component$ } from "@builder.io/qwik";
-import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik';
+import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
 
 /**
- * The RouterHead component is placed inside of the document `<head>` element.
+ * Componente RouterHead, responsável por preencher o conteúdo da tag <head>.
+ *
+ * Ele extrai as informações de título, metas, links, estilos e scripts
+ * através do hook useDocumentHead() e, opcionalmente, adiciona a tag
+ * canonical via useLocation() para SEO.
  */
 export const RouterHead = component$(() => {
   const head = useDocumentHead();
@@ -10,37 +14,47 @@ export const RouterHead = component$(() => {
 
   return (
     <>
-      <title>{head.title}</title>
+      {/* Título da página */}
+      {head.title && <title>{head.title}</title>}
 
+      {/* Link canônico para SEO */}
       <link rel="canonical" href={loc.url.href} />
+
+      {/* Metadados e favicon básicos */}
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
+      {/* Metas dinâmicas declaradas nas páginas */}
       {head.meta.map((m) => (
-        <meta key={m.key} {...m} />
-      ))}
-
-      {head.links.map((l) => (
-        <link key={l.key} {...l} />
-      ))}
-
-      {head.styles.map((s) => (
-        <style
-          key={s.key}
-          {...s.props}
-          {...(s.props?.dangerouslySetInnerHTML
-            ? {}
-            : { dangerouslySetInnerHTML: s.style })}
+        <meta
+          key={m.name ?? m.property ?? m.content}
+          {...m}
         />
       ))}
 
+      {/* Links dinâmicos (preloads, prefetcheados etc.) declarados nas páginas */}
+      {head.links.map((l) => (
+        <link
+          key={l.href ?? l.rel}
+          {...l}
+        />
+      ))}
+
+      {/* Estilos dinâmicos gerados pelo Qwik City */}
+      {head.styles.map((s) => (
+        <style
+          key={s.props?.id ?? s.props?.nonce}
+          {...s.props}
+          dangerouslySetInnerHTML={s.style}
+        />
+      ))}
+
+      {/* Scripts dinâmicos definidos via DocumentHead.scripts */}
       {head.scripts.map((s) => (
         <script
-          key={s.key}
+          key={s.props?.id ?? s.props?.nonce}
           {...s.props}
-          {...(s.props?.dangerouslySetInnerHTML
-            ? {}
-            : { dangerouslySetInnerHTML: s.script })}
+          dangerouslySetInnerHTML={s.script}
         />
       ))}
     </>
